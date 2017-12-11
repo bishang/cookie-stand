5cdb5675a5a96cf8e8e5f621856848673ece1616
Bessie Arino <brarino@gmail.com>
2017-12-11 15:46:17 -0800

    added for loop to store in array

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
 'use strict';
 
+var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm']
+
 var firstPike = {
   location: '1st and Pike',
   minCust: 23,
   maxCust: 65,
   avgCookieSale: 6.3,
+  cookiesPerHour: [],
   custPerHour: function(minCust, maxCust) {
     return Math.random() * (this.maxCust - this.minCust) + this.minCust;
-  }
+  },
+  cookiesPurchased: function() {
+    return this.custPerHour() * this.avgCookieSale}
+}
+
+for(var i = 0; i < hours.length; i++) {
+  firstPike.cookiesPerHour.push(firstPike.cookiesPurchased());
 }
 
 var seatacAirport = {
