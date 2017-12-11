6ba782a058acdcd78063fdc162745b30240d0e7b
Bessie Arino <brarino@gmail.com>
2017-12-11 13:31:08 -0800

    added object litterals

---
+++ app.js
@@ -0,0 +1,36 @@
+'use strict';
+
+var firstPike = {
+  location: '1st and Pike',
+  minCust: 23,
+  maxCust: 65,
+  avgCookieSale: 6.3
+}
+
+var seatacAirport = {
+  location: 'SeaTac Airport',
+  minCust: 3,
+  maxCust: 24,
+  avgCookieSale: 1.2
+}
+
+var seattleCenter = {
+  location: 'Seattle Center',
+  minCust: 11,
+  maxCust: 38,
+  avgCookieSale: 3.7
+}
+
+var capitolHill = {
+  location: 'Capitol Hill',
+  minCust: 20,
+  maxCust: 38,
+  avgCookieSale: 2.3,
+}
+
+var alki = {
+  location: 'Alki',
+  minCust: 2,
+  maxCust: 16,
+  avgCookieSale: 4.6
+}
