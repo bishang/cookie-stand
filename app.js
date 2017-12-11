6ed29b77df26feec9555b31f549353c7600b2b87
Bessie Arino <brarino@gmail.com>
2017-12-11 14:05:20 -0800

    added function to generate random customers and added html to html files

--- app.js
+++ app.js
@@ -4,21 +4,30 @@ var firstPike = {
   location: '1st and Pike',
   minCust: 23,
   maxCust: 65,
-  avgCookieSale: 6.3
+  avgCookieSale: 6.3,
+  custPerHour: function(minCust, maxCust) {
+    return Math.random() * (this.maxCust - this.minCust) + this.minCust;
+  }
 }
 
 var seatacAirport = {
   location: 'SeaTac Airport',
   minCust: 3,
   maxCust: 24,
-  avgCookieSale: 1.2
+  avgCookieSale: 1.2,
+  custPerHour: function(minCust, maxCust) {
+    return Math.random() * (this.maxCust - this.minCust) + this.minCust;
+  }
 }
 
 var seattleCenter = {
   location: 'Seattle Center',
   minCust: 11,
   maxCust: 38,
-  avgCookieSale: 3.7
+  avgCookieSale: 3.7,
+  custPerHour: function(minCust, maxCust) {
+    return Math.random() * (this.maxCust - this.minCust) + this.minCust;
+  }
 }
 
 var capitolHill = {
@@ -26,11 +35,17 @@ var capitolHill = {
   minCust: 20,
   maxCust: 38,
   avgCookieSale: 2.3,
+  custPerHour: function(minCust, maxCust) {
+    return Math.random() * (this.maxCust - this.minCust) + this.minCust;
+  }
 }
 
 var alki = {
   location: 'Alki',
   minCust: 2,
   maxCust: 16,
-  avgCookieSale: 4.6
+  avgCookieSale: 4.6,
+  custPerHour: function(minCust, maxCust) {
+    return Math.random() * (this.maxCust - this.minCust) + this.minCust;
+  }
 }
